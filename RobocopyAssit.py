e84f521e4df8de03ad8636139bdc12d594fc2545
Nicolas <nicolastroupel@outlook.com>
2022-02-10 19:21:06 +0100

    Mise à jour gobale du script.

--- RobocopyAssit.py
+++ RobocopyAssit.py
@@ -1,140 +1,200 @@
 # RobocopyAssit
 # Nicolas Troupel
-# version: 20211011
+# version: 20220210
 
+import datetime
 import inspect
+import json
 import pathlib
 import platform
 import subprocess
 import sys
-import os
 
-# Gére le soulèvement d'exception. La fonction récupère
-# les informations relative à l'exception afin de poursuivre l'arret
-# ou d'affiche ces informations. Par défault, la fonction termine dès qu'une
-# exception est soulevé.
-def handle_func_exception(exit=True, exit_value="1"):
-	try:
-		exc_info = sys.exc_info()
-		exc_type = str(exc_info[0])[8:-2]
-		exc_value = exc_info[1]
-		exc_trace = inspect.trace()
-		exc_file = None
-		exc_line = None
-		exc_name = None
+# Gére l'affichage pour le debug du script. 
+def debug(text):
+	print("{:>^80}".format("DEBUG!"))
+	print(text)
+	print("{:<^80}".format("DEBUG!"))
 
-		if exc_type == "SystemExit":
-			raise(SystemExit(exc_value))
-		else:
-			print("{:-<80}".format(""))
-			for i in exc_trace:
-				exc_file = i[1]
-				exc_line = i[2]
-				exc_name = i[3]
-				print("In file {} at the line #{}"\
-				.format(exc_file, exc_line))
-				print("{:<3}{} had met an type error {}"\
-				.format("", exc_name, exc_type))
-				if exc_value:
-					print("{:<3}{}".format("", exc_value))
-				print("Error code: {0}".format(exit_value))
+# Gére le soulèvement d'exception. Par défault, la fonction termine dès qu'une
+# exception est soulevé avec le code d'erreur 0.
+def handle_exception(to_exit=True, error_code=0):
+	# Récupère une liste non modifiable (tuple) regroupant des informations
+	# sur l'exception soulevée.
+	exc_info = sys.exc_info()
+	# debug(exc_info)
+	# Récupère le non de la classe qui a soulevé l'exception.
+	exc_type = exc_info[0]
+	# Récupère la valeur de l'exception.
+	exc_value = exc_info[1]
+	# Récupère une liste regroupant des informations relative au cadre.
+	exc_trace = inspect.trace()
+	# debug(exc_trace)
+	# Définie une variable contenant le nom du fichier
+	# où est soulevé l'exception.
+	exc_file = None
+	# Définie une variable contenant la ligne du fichier
+	# où est soulevé l'exception.
+	exc_line = None
+	# Définie une variable contenant le nom de la fonction
+	# où est soulevé l'exception
+	exc_name = None
 
-		if exit:
+	if exc_type != SystemExit:
+		exc_file = exc_trace[-1][1]
+		exc_line = exc_trace[-1][2]
+		exc_name = exc_trace[-1][3]
+
+		print("{:-<80}".format(""))
+		print("In file {} at the line #{}".format(exc_file, exc_line))
+		print("{:<3}{} had met an error {} of type {}"\
+			.format("", exc_name, error_code, exc_type))
+		if exc_value:
+			print("{:<3}because {}".format("", exc_value))
+		print("{:-<80}".format(""))
+
+		if (to_exit):
 			input("Press Enter to quit...")
-			raise(SystemExit(exit_value))
+			sys.exit(error_code)
 
-	except SystemExit as value:
-		sys.exit(value)
+	else:
+		sys.exit(error_code)
 
 # Vérrifie que le système d'exploitation est Windows
 # et que la version 3 de Python est utilisé. Si cela n'est pas le cas,
-# la fonction termine.
+# la fonction termine le script.
+# Code d'erreurs: 20; 21.
 def check_system():
+	print("Check system...")
+
 	try:
 		if platform.system() != "Windows":
+			# debug(platform.system())
 			raise OSError("Windows is required")
+	except:
+		handle_exception(error_code=20)
 
-		if int(platform.python_version_tuple()[0]) != 3:
+	try:
+		if int(platform.python_version_tuple()[0]) < 3:
+			# debug(platform.python_version_tuple)
 			raise SystemError("Python 3 is required")
-
 	except:
-		handle_func_exception(exit_value="02")
+		handle_exception(error_code=21)
 
-# A partir du chemin du fichier en cours d'execution,
-# la fonction retourne dans une tulpe le nom du lecteur,
-# le dossier où se trouve le fichier et le chemin du fichier.
-# Ce est dernier est passé en argument par la variable frame
-# via la fonction inspect.currentframe().
-def check_file_location(frame=None, root=None):
+	print("System checked!")
+
+# A partir du chemin du dossier de travail,
+# indique si le dossier est à la racine du lecteur,
+# en retournant le cas échant son chemin.
+# Code d'erreur: 30.
+def get_source():
+	print("Get Source...")
+
 	try:
-		frame_path = pathlib.WindowsPath(os.getcwd())
-		directory_path = frame_path.parent
+		directory_path = pathlib.WindowsPath.cwd()
+		# debug(directory_path)
 
-		if root and directory_path != pathlib.WindowsPath(frame_path.anchor):
-			raise UserWarning("{} don't found at the root of drive {}"\
-			.format(frame_path.name, frame_path.drive))
-
-		result = (frame_path.drive, str(directory_path), str(frame_path))
+		if not directory_path.parent.match(directory_path.anchor):
+			raise UserWarning("{} don't found in a directory at the root of drive {}"\
+				.format(directory_path.name, directory_path.drive))
 
 	except:
-		handle_func_exception(exit_value="03")
-	else:
-		return result
+		handle_exception(error_code=30)
 
-# Si la variable source est True, récupère à partir de la lettre d'un lecteur
-# son nom et vérifie que celui ne se termine pas par -cop, sinon la fonction
-# déclenche une exception.
-# Si la variable source est False, récupère à partir du nom
-# auquels "-cop" est ajouté la lettre d'un lecteur.
-# Vérifie aussi que le lecteur est sain.
-def get_volume(source=None, letter=None, label=None):
+	else:
+		print("SourceGotten!")
+		return directory_path.anchor
+
+# Récupère à partir du lecteur source le lecteur de destination en y ajoutant
+# -bak. Vérifie aussi que la santé des lecteurs.
+# Code d'erreurs: 40, 41, 42, 43.
+def get_destination_from_source(sourcePath):
+	print("Get Destination...")
+
 	try:
-		if source and letter:
-			if letter.endswith(":"):
-				letter = letter[:-1]
-			infos = subprocess.check_output(["powershell", "get-volume",\
-			"-driveletter", letter]).decode(sys.stdout.encoding).split()
-			label = infos[17]
-			if label.endswith("-bak"):
-				raise UserWarning("{} is not the source volume".format(label))
+		sourceInfos = subprocess.check_output(["powershell", "get-volume",\
+				"-FilePath", sourcePath, "|", "ConvertTo-Json"])\
+				.decode(sys.stdout.encoding, errors="ignore")
+		# debug(sourceInfos)
 
-		elif not source and label:
-			label = "{}-bak".format(label)
-			try:
-				infos = subprocess.check_output(["powershell", "get-volume",\
-				"-filesystemlabel", label]).decode(sys.stdout.encoding).split()
-			except:
-				raise UserWarning("The volume {} is not found".format(label))
-			letter = infos[16]
+		sourceInfos = json.loads(sourceInfos)
+		# debug(sourceInfos)
 
-		else:
-			raise UserWarning("The arguments are not valid")
+		# debug(sourceInfos["FileSystemLabel"])
+		sourceLabel = sourceInfos["FileSystemLabel"]
 
-		health = infos[20]
-		if health != "Healthy":
-			raise UserWarning("The {} is maybe endomaged"\
-			.format(label))
-
-		result = ("{}:".format(letter), label)
-
+		# debug(sourceInfos["HealthStatus"])
+		sourceHealthStatus = sourceInfos["HealthStatus"]
 	except:
-		handle_func_exception(exit_value="04")
-	else:
-		return result
+		handle_exception(error_code=40)
 
-# Déclenche Robocopy en excluant le dossier "System Volume Information".
-# La sortie est affiché en temps réel. Une exception est déclenché si Robocopy
+
+	try:
+		if sourceHealthStatus != "Healthy":
+			raise UserWarning("{} is {}".format(sourceLabel, sourceHealthStatus))
+	except:
+		handle_exception(error_code=41)
+
+	destinationLabel = "{}-bak".format(sourceLabel)
+	# debug(destinationLabel)
+
+	try:
+		destinationInfos = subprocess.check_output(["powershell", "get-volume",\
+				"-FileSystemLabel", destinationLabel, "|", "ConvertTo-Json"])\
+				.decode(sys.stdout.encoding, errors="ignore")
+		# debug(destinationInfos)
+
+		destinationInfos = json.loads(destinationInfos)
+		# debug(destinationInfos)
+
+		if (isinstance(destinationInfos, list) and len(destinationInfos) > 1):
+			raise UserWarning("There is two or more drive with same destination label.")
+
+		# debug(destinationInfos["DriveLetter"])
+		destinationDriveLetter = destinationInfos["DriveLetter"]
+
+		# debug(destinationInfos["HealthStatus"])
+		destinationHealthStatus = destinationInfos["HealthStatus"]
+	except:
+		handle_exception(error_code=42)
+
+	try:
+		if destinationHealthStatus != "Healthy":
+			raise UserWarning("{} is {}".format(sourceLabel, sourceHealthStatus))
+	except:
+		handle_exception(error_code=43)
+
+	print("Destination Gotten!")
+
+	return ("{}:\\".format(destinationDriveLetter), destinationLabel, sourceLabel)
+
+# Déclenche Robocopy en excluant le dossier "System Volume Information",
+# la corbeille et le dossier LOG à la racine de la source.
+# La sortie est affiché en temps réel et dans un fichier log.
+# Une exception est déclenché si Robocopy
 # termine en renvoyant une valeur supérieur à 3.
-def run_robocopy(src, dst):
+# Code d'erreur: 50.
+def run_robocopy(src_path, dst_path):
+	print("Start Robocopy...")
+
 	try:
-		robocopy = subprocess.Popen(["robocopy", "{0}\\".format(src), "{0}\\".format(dst),\
-		"*.*", "/dcopy:T", "/copyall", "/secfix", "/timfix", "/mir", "/a+:RA",\
-		"/xd", "System Volume Information", "$RECYCLE.BIN", "/it", "/xj", "/fft",\
-		"/r:1", "/w:2", "/v", "/ts", "/bytes"],\
+		log_file = pathlib.PureWindowsPath(src_path, "LOG", "robocopy-{}.log"\
+		.format(datetime.datetime.now().strftime("%Y-%m-%dT%H-%M-%S")))
+		# debug(log_file)
+
+		robocopy = subprocess.Popen(["robocopy", src_path, dst_path,\
+		# Options de copie
+		"/e", "/zb", "/dcopy:dat", "/copyall", "/secfix", "/timfix", "/mir", "/a+:RA", "/a-:SHNT",\
+		# Options de sélections de fichier
+		"/xd", "System Volume Information", "$RECYCLE.BIN", "LOG",\
+		# Options de nouvelle tentative
+		"/r:3", "/w:10",\
+		# Options du journal
+		"/v", "/ts", "/fp", "/bytes", "/log:{}".format(log_file), "/tee"],\
 		stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 
 		while robocopy.returncode == None:
-			line = robocopy.stdout.readline().decode("cp850").rstrip('\n')
+			line = robocopy.stdout.readline().decode("cp850", errors="ignore").rstrip('\n')
 			if not line:
 				robocopy.wait()
 			else:
@@ -145,39 +205,40 @@ def run_robocopy(src, dst):
 			.format(robocopy.returncode))
 
 	except:
-		handle_func_exception(exit_value="05")
+		handle_exception(error_code=50)
+	else:
+		print("Robocopy finished!")
 
 # Ceci s'execute lorsque le fichier est dirrectement éxécuté.
 if __name__ == "__main__":
+	print("{:-^80}".format("RobocopyAssit"))
+	print("{:-^80}".format("Version 20220210"))
+
+	check_system()
+	print("{:-<80}".format(""))
+
+	source = get_source()
+	# debug(source)
+	print("{:-<80}".format(""))
+
+	destination = get_destination_from_source(source)
+	# debug(destination)
+	print("{:-<80}".format(""))
+
 	try:
-		print("RobocopyAssit")
-		print("Version 20211011")
-
-		print("Check system...")
-		check_system()
-
-		print("Research volumes...")
-		file_location = check_file_location(frame=inspect.currentframe(), root=False)
-		source = get_volume(source=True, letter=file_location[0])
-		destination = get_volume(source=False, label=source[1])
-
-		print("Bakup from \"{0}\" to \"{1}\"...".format(source[1], destination[1]))
-
-		print("Quit (tape q) or Continue (press Enter)...")
-		keys = input()
-		if keys == 'q':
-			raise UserWarning("User has quit bakup.")
-		
-
-		run_robocopy(source[0], destination[0])
-
-		print("Bakup is complete.")
-
+		to_start = input("Start bakup from \"{}\" ({}) to \"{}\" ({})...\n\
+Press y (yes) to continue or any key to quit...\n"\
+		.format(destination[2], source, destination[1], destination[0]))
+		# debug(to_start)	
+		if not (to_start.lower() == "y" or to_start.lower() == "yes"):
+			raise SystemExit("User has quit bakup.")
 	except:
-		handle_func_exception(exit_value="06")
+		handle_exception(60)
 	else:
 		print("{:-<80}".format(""))
 
-		input("Press Enter for quit...")
+	run_robocopy(source, destination[0])
+	print("{:-<80}".format(""))
 
-		sys.exit(0)
+	print("Bakup completed!")
+	
